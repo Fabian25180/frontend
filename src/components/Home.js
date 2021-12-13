0a598ceb9e84b5cff6d7bc612e698d9947530ca6
Janis-Gehrig <janis.gehrix@gmail.com>
2021-12-13 18:32:56 +0100

    Co-authored-by: JujuEzay <JujuEzay@users.noreply.github.com>

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class CustomSettings extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hallo</h1>
+            </div>
+        )
+    }
+}
