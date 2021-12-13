f8010dbcd21a5812d635dd382dd125324c77984d
Janis-Gehrig <janis.gehrix@gmail.com>
2021-12-13 19:14:22 +0100

    Co-authored-by: JujuEzay <JujuEzay@users.noreply.github.com>

--- src/components/NavbarComp.js
+++ src/components/NavbarComp.js
@@ -18,39 +18,46 @@ export default class NavbarComp extends Component {
             <Router>
                 <div>
                     <Navbar bg="dark" variant={"dark"} expand="lg">
-                        <Container fluid>
+                        <Container fluid-left>
                             <Navbar.Brand href="#">Projekt22</Navbar.Brand>
                             <Navbar.Toggle aria-controls="navbarScroll" />
                             <Navbar.Collapse id="navbarScroll">
+                            <Container fluid>
                                 <Nav
-                                    className="me-auto my-2 my-lg-0"
+                                    className=" ml- my-2 my-lg-0 "
                                     style={{ maxHeight: '100px' }}
                                     navbarScroll
                                 >
                                     <Nav.Link as={Link} to="/home" >Home</Nav.Link>
-                                    
-                                    <Nav.Link as={Link} to="/settings">Settings</Nav.Link>
-                                    <NavDropdown title="Link" id="navbarScrollingDropdown">
-                                        <NavDropdown.Item as={Link} to="/custom-settings">Custom-Settings</NavDropdown.Item>
-                                        <NavDropdown.Item as={Link} to="/notification" >Notification</NavDropdown.Item>
-                                        <NavDropdown.Divider />
-                                        <NavDropdown.Item href="#action5">
-                                            Something else here
-                                        </NavDropdown.Item>
-                                    </NavDropdown>
-                                    <Nav.Link href="#" disabled>
-                                        Link
-                                    </Nav.Link>
-                                </Nav>
-                                <Form className="d-flex">
+                                </Nav>   
+                            </Container>
+                            <Container fluid class="mx-auto w-35 p-3" >
+                            <Form className="d-flex">
                                     <FormControl
                                         type="search"
                                         placeholder="Search"
                                         className="me-2"
                                         aria-label="Search"
+                                        
                                     />
                                     <Button variant="outline-success">Search</Button>
-                                </Form>
+                            </Form>
+                            </Container>
+                            <Container fluid-right>
+                                <Nav>
+                                    <Nav.Link as={Link} to="/settings">Settings</Nav.Link>
+                                            <NavDropdown title="Link" id="navbarScrollingDropdown">
+                                                <NavDropdown.Item as={Link} to="/custom-settings">Custom-Settings</NavDropdown.Item>
+                                                <NavDropdown.Item as={Link} to="/notification" >Notification</NavDropdown.Item>
+                                                <NavDropdown.Divider />
+                                                <NavDropdown.Item href="#action5">
+                                                    Something else here
+                                                </NavDropdown.Item>
+                                            </NavDropdown>
+                                    <Nav.Link href="#" disabled>Link</Nav.Link>
+                                    <Nav.Link as={Link} to="/notifications">notifications</Nav.Link>
+                                </Nav>
+                            </Container>                 
                             </Navbar.Collapse>
                         </Container>
                     </Navbar>
